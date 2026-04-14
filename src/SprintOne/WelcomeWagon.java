dab5b3833f7e00573874b4f3bccce0e448d070fe
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-04-14 16:46:30 +0400

    Solving Second Task Welcome Wagon

---
+++ src/SprintOne/WelcomeWagon.java
@@ -0,0 +1,7 @@
+package SprintOne;
+
+public class WelcomeWagon {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java Programming \nLearn, Practice, Succeed \nHappy Coding!");
+    }
+}
