94eb1234af5d6186b8f7788036f32e528fa98417
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-04-14 17:02:40 +0400

    Solving the task #5

---
+++ src/SprintOne/StarFrame.java
@@ -0,0 +1,12 @@
+package SprintOne;
+
+public class StarFrame {
+
+    public static void main(String[] args) {
+
+        System.out.println("***************************\n*\t                      *");
+        System.out.println("*\t\"Keep The Momentum\"\t  *");
+        System.out.println("*\t                      *\n***************************");  //Reverse of line 4
+
+    }
+}
