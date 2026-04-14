c5fb418530f80bddfbc676de1098a340c3f166e2
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-04-14 16:56:03 +0400

    Solving the task #3

---
+++ src/SprintOne/TalkTalk.java
@@ -0,0 +1,9 @@
+package SprintOne;
+
+public class TalkTalk {
+    public static void main(String[] args){
+        System.out.println("Teacher: \"What is your name?\"");
+        System.out.println("Student: \"My name is Ibrahim\"");
+        System.out.println("Teacher: \"Welcome, Ibrahim!\"");
+    }
+}
