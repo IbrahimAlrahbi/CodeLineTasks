c149507d892dd5c79a7c023ecdc69b5cf555e272
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-04-15 11:12:43 +0400

    Creating the class & map

---
+++ src/EvaluationTasks/SnakeMove.java
@@ -0,0 +1,12 @@
+package EvaluationTasks;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class SnakeMove {
+    public static void main(String[] argumentsInput) throws IOException {
+
+    }
+}
