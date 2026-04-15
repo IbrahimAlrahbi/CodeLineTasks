e79217c4cf3871427a72964292ad8c5b03996e40
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-04-15 11:14:49 +0400

    Addding Validation Part for CLI

--- src/EvaluationTasks/SnakeMove.java
+++ src/EvaluationTasks/SnakeMove.java
@@ -8,5 +8,17 @@ import java.util.Scanner;
 public class SnakeMove {
     public static void main(String[] argumentsInput) throws IOException {
 
+        //  Part 1. Validate CLI user input
+
+        //  Validate Arguments
+        if (argumentsInput.length == 0) {
+            System.out.println("Error: Must run via command line with: java MoveSnake.java <direction> <steps> ");
+            return;
+        }
+
+        if (argumentsInput.length > 2) {
+            System.out.println("Error: Input Must Be java MoveSnake <direction> <steps>");
+            return;
+        }
     }
 }
